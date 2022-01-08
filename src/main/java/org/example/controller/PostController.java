a535e8fc45aeb533b3515db4e92cc0fd9dc32ce1
shmelevvv <shmelevvv@outlook.com>
2022-01-08 12:44:05 +0300

    First commit

--- src/main/java/org/example/controller/PostController.java
+++ src/main/java/org/example/controller/PostController.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import org.example.exception.NotFoundException;
 import org.example.model.Post;
 import org.example.service.PostService;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -11,7 +12,10 @@ import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
     public static final String APPLICATION_JSON = "application/json";
     private final PostService service;
@@ -21,45 +25,24 @@ public class PostController {
         this.service = service;
     }
 
-    public void all(HttpServletResponse response) throws IOException {
-        final var data = service.all();
-        Post[] posts = data.toArray(new Post[0]);
-        makeSuccessResponse(response, posts);
+    @GetMapping
+    public List<Post> all() {
+        return service.all();
     }
 
-    public void getById(long id, HttpServletResponse response) throws IOException{
-        try {
-            final var data = service.getById(id);
-            makeSuccessResponse(response, data);
-        } catch (NotFoundException e) {
-            makeFailResponse(response);
-        }
+    @GetMapping("/{id}")
+    public Post getById(@PathVariable long id) {
+        return service.getById(id);
     }
 
-    public void save(Reader body, HttpServletResponse response) throws IOException {
-        try {
-            final var post = gson.fromJson(body, Post.class);
-            final var data = service.save(post);
-            makeSuccessResponse(response, data);
-        } catch (NotFoundException e) {
-            makeFailResponse(response);
-        }
+    @PostMapping
+    public Post save(@RequestBody Post post) {
+        return service.save(post);
     }
 
-    public void removeById(long id, HttpServletResponse response) throws IOException {
+    @DeleteMapping("/{id}")
+    public void removeById(long id) {
         service.removeById(id);
-        makeSuccessResponse(response, null);
-    }
-
-    private void makeSuccessResponse(HttpServletResponse response, Post... posts) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        response.setStatus(200);
-        response.getWriter().print(gson.toJson(posts));
-    }
-
-    private void makeFailResponse(HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        response.setStatus(400);
     }
 
 }
